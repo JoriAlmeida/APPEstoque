b8e821a85de9caf788b6f821a63475cf7832e0e6
JoriAlmeida <85134005+JoriAlmeida@users.noreply.github.com>
2024-07-04 00:01:24 -0300

    Add files via upload

---
+++ APP ESTOQUE/BackEnd/src/main/java/gerenteinteligente/estoque/gerenteinteligente/GerenteinteligenteApplication.java
@@ -0,0 +1,13 @@
+package gerenteinteligente.estoque.gerenteinteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenteinteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenteinteligenteApplication.class, args);
+	}
+
+}
---
+++ APP ESTOQUE/BackEnd/src/test/java/gerenteinteligente/estoque/gerenteinteligente/GerenteinteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenteinteligente.estoque.gerenteinteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenteinteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
